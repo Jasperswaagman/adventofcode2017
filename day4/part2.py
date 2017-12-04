7e7b6a6ae1e0a3d9851b805bba04b542a26f54e8
Jasper Swaagman <jasper.swaagman@belsimpel.nl>
2017-12-04 20:19:59 +0100

    wiih

--- day4/part2.py
+++ day4/part2.py
@@ -1,23 +1,27 @@
 from itertools import permutations
-with open('smallinput', 'r') as f:
+with open('input', 'r') as f:
   passphrases = f.readlines()
 
 passphrases = [r.strip() for r in passphrases]
+n = 0
 
 def has_double(s):
   seen = set()
   for w in s:
-    if w in seen: return
+    if w in seen: return True
     seen.add(w)
 
 def has_anagram(s):
   if s:
     perms = [''.join(p) for p in permutations(s[0])]
     for _ in perms:
-      if _ in s[1:]: print('DAFUQ is dit'); return 1
-    has_anagram(s[1:]) 
+      if _ in s[1:]: 
+        return True
+    return has_anagram(s[1:]) 
 
 for l in passphrases:
-  if not has_double(l.split()):
-    print(has_anagram(l.split()))
+  l = l.split()
+  if not has_double(l): 
+    if not has_anagram(l): n += 1
 
+print(n)
