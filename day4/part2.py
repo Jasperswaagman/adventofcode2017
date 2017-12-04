3f4167b73f738e6b83beaba97ad90aca4e46a9eb
Jasper Swaagman <jasper.swaagman@belsimpel.nl>
2017-12-04 19:44:57 +0100

    weird ass thingies

---
+++ day4/part2.py
@@ -0,0 +1,25 @@
+from itertools import permutations
+with open('smallinput', 'r') as f:
+  passphrases = f.readlines()
+
+passphrases = [r.strip() for r in passphrases]
+n = 0
+
+def has_double(s):
+  global n
+  seen = set()
+  for w in s:
+    if w in seen: return
+    seen.add(w)
+
+def has_anagram(s):
+  if s:
+    perms = [''.join(p) for p in permutations(s[0])]
+    for _ in perms:
+      if _ in s[1:]: print('DAFUQ is dit'); return 1
+    has_anagram(s[1:]) 
+
+for l in passphrases:
+  if not has_double(l.split()):
+    print(has_anagram(l.split()))
+
