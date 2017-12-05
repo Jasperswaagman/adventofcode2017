d657052f573dc4cab924995e9a0a9339d6709f3d
Jasper Swaagman <jasperswaagman@gmail.com>
2017-12-05 23:53:58 +0100

    fixed

--- day5/part1.py
+++ day5/part1.py
@@ -1,14 +1,12 @@
-with open('smallinput') as f:
+with open('input') as f:
   instructions = [int(p.strip()) for p in f.readlines()]
 
-n = 0
-i = instructions[0]
+steps, i = 0, 0
 
 while i < len(instructions):
   prev_i = i
   i += instructions[i]
   instructions[prev_i] += 1 
-  print(instructions)
-  n += 1
+  steps += 1
 
-print(n)
+print(steps)
--- day5/part2.py
+++ day5/part2.py
@@ -1,13 +1,12 @@
-with open('smallinput') as f:
+with open('input') as f:
   instructions = [int(p.strip()) for p in f.readlines()]
 
-n = 0
-i = instructions[0]
+n, i = 0, 0
 
 while i < len(instructions):
   prev_i = i
   i += instructions[i]
-  if instructions[prev_i] > 2:
+  if instructions[prev_i] >= 3:
     instructions[prev_i] -= 1 
   else:
     instructions[prev_i] += 1 
